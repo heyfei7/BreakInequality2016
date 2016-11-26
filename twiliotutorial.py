05080a84c67460099922c46d8abbf7da62a88348
Fei Dong <feidong1998@gmail.com>
2016-11-26 14:02:07 -0500

    Update twiliotutorial.py

--- twiliotutorial.py
+++ twiliotutorial.py
@@ -1,15 +1,15 @@
 from twilio.rest import TwilioRestClient
-    # put your own credentials here
-    ACCOUNT_SID = '<AccountSid>'
-    AUTH_TOKEN = '<AuthToken>'
+# put your own credentials here
+ACCOUNT_SID = '<AccountSid>'
+AUTH_TOKEN = '<AuthToken>'
 
-    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
-    client.messages.create(
-        to = '<ToNumber>',
-        from_ = '<FromNumber>',
-        body = '<BodyText>',
-    )
+client.messages.create(
+    to = '<ToNumber>',
+    from_ = '<FromNumber>',
+    body = '<BodyText>',
+)
            
 # Using Flask
 #########################################################################
