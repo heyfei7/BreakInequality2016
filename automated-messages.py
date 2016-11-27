48f0bec6ccf1145a69eb4fede84e55d75f4da5d7
Fei Dong <feidong1998@gmail.com>
2016-11-26 20:50:50 -0500

    Update automated-messages.py

--- automated-messages.py
+++ automated-messages.py
@@ -9,7 +9,7 @@ client = TwilioRestClient(account_sid, auth_token)
 
 phone_book ={"Fei":"6472333048",
             "Anupya":"6479940655",
-            "Hu Yue":"",
+            "Hu Yue":"5195888188",
             "Fizaa":"",
             "Ankita":"",
             "Shreya":""}
@@ -22,25 +22,30 @@ def make_symptoms():
 
 weekly_symptoms = make_symptoms()
 
-def send_sms(number,message):
+def send_sms(number,msg,url):
     client.messages.create(
         to = number,
         from_ = n,
-        body = message,
+        body = msg,
+        media_url = url,
         )
 
 def find_week(duedate,today):
-    difference=duedate-today
-    total_days=280
-    days_elapsed=total_days-difference.days
-    week=days_elapsed//7+1
-    return week
+    difference = duedate-today
+    total_days = 280
+    days_elapsed = total_days-difference.days
+    week = (days_elapsed//7)+1
+    symptoms = weekly_symptoms[week]
+    if week==1:
+        url="http://www.pregnancycorner.com/wp-content/uploads/1-week-pregnant.jpg"
+    else:
+        url="http://www.pregnancycorner.com/wp-content/uploads/"+str(week)+"-weeks-pregnant.jpg"
+    return [symptoms,url]
 
 def send_weekly_message(duedate,number):
     today = datetime.date.today()
-    week = find_week(duedate,today)
-    msg=weekly_symptoms[week]
-    send_sms(number,msg)
+    week_info = find_week(duedate,today)
+    send_sms(number,week_info[0],week_info[1])
 
 def main(due_year,due_month,due_day,number):
     duedate=datetime.date(due_year,due_month,due_day)
